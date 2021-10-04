8cde2622622df0983dc245071accb48a288cfeec
Jayanth Saikiran <jayanthsaikiran@gmail.com>
2021-10-04 20:18:57 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -20,7 +20,7 @@ const Projects = () => {
                 </div>
                 <div className="projectCard" onClick={() => window.open("https://play.google.com/store/apps/details?id=com.gfeed.sudhaseshu.gitamfeed", "_blank")}>
 
-                  >
+
                     <p className="color-blue projectTitle">Gitam Feed</p>
                     <p className="color-black projectDesc">College feed and community</p>
                     <p className="color-grey projectDomain">Android Application</p>
