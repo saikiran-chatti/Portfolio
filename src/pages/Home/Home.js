9a83dc11ce463a9b61d5a4302cb492f47cd19ef8
jayanthsaikiran <jayanthsaikiran@gmail.com>
2021-10-10 23:15:19 +0530

    restricted opening developer options

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import Intro from '../../components/intro/Intro'
@@ -7,6 +7,45 @@ import Projects from '../../components/Projects/Projects'
 import Footer from '../../components/Footer/Footer'
 
 const Home = () => {
+
+    const handleContextMenu = useCallback(
+        (e) => {
+            e.preventDefault();
+        }, []
+    );
+
+    useEffect(() => {
+        document.addEventListener("contextmenu", handleContextMenu);
+
+        document.addEventListener('keydown', function (e) {
+            console.log(e.key);
+            if (e.key === 123) {
+                e.preventDefault();
+                return false;
+            }
+            if (e.ctrlKey && e.shiftKey && e.key === 'I') {
+                e.preventDefault();
+                return false;
+            }
+            if (e.ctrlKey && e.shiftKey && e.key === 'C') {
+                e.preventDefault();
+                return false;
+            }
+            if (e.ctrlKey && e.shiftKey && e.key === 'J') {
+                e.preventDefault();
+                return false;
+            }
+            if (e.ctrlKey && e.key === 'U') {
+                e.preventDefault();
+                return false;
+            }
+        });
+
+        return () => {
+            document.removeEventListener("contextmenu", handleContextMenu);
+        };
+    });
+
     return (
         <div className="home">
             <div className="blueGradientTop">
