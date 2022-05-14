3347461ae01e8c6dc31bfc504f1f74e80d87351e
Jayanth Saikiran <jayanthsaikiran@gmail.com>
2022-05-14 13:41:12 +0530

    removed warnings

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react'
+import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import Intro from '../../components/intro/Intro'
